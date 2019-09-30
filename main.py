d5afe361a2e3a43527745e77e36ff91dcc2605e0
c-lang18 <carolinalang96@gmail.com>
2019-09-30 23:40:14 +1000

    Update renaming

--- main.py
+++ main.py
@@ -3,37 +3,27 @@ from PIL import Image
 import filetype
 import pathlib
 
-"""
-def determine_ownership():
-    dir = os.getcwd()
-
-    for filename in glob.glob("*.JPG"):
-        img = Image.open(filename)
-        img.save('_MINE_' + str(filename))
-
-"""
-
 dir = os.listdir(os.getcwd())
 
 def checkFile():
 
-    for files in dir:
-        #ext = files[-3:]
-        ext = files.split('.')[1]
+    for file in os.listdir("."):
+        ext = file.split('.')[1]
+        newName = '__MINE__'+file
 
         if ext == 'png' or ext == 'jpg':
-            print('theoretically renamed: {0}'.format(files))
+            os.rename(file, file.replace(newName, '', 1))
+            print(file)
         else:
-            print("{0} ignored".format(files))
+            print("|")
+    print(newName)
 
 
 if __name__=='__main__':
     checkFile()
 
-##working on 1 format
 # TODO
 # instead of copying have the file rename existing files
-# work on multiple formats
 
 #- browse the current directory (dir)
 #- check file extension
