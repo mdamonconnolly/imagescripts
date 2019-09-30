7d199a0641ad991d366533521de78cee67c674d4
c-lang18 <carolinalang96@gmail.com>
2019-09-30 23:55:42 +1000

    Renaming script
    - Final code
    - Works on .pngs and .jpgs
    - Refactored

--- README.md
+++
@@ -1,5 +0,0 @@
-# imageResizer
-Very basic script to batch-rename reference images in a folder to assign ownership
-TODO
-- add functionality to resize images
-- potentially basic TKinter UI
--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import os
-from PIL import Image
-
+#from PIL import Image
 
 def check_and_rename():
-
     for file in os.listdir("."):
-        ext = file.split('.')[1]
-        newName = '__MINE__'+file
+        newName = '__MINE__' + file
 
         if file.endswith(".png") or file.endswith(".jpg"):
             os.rename(file, newName)
-            print(file)
-        else:
-            print("|")
 
 if __name__=='__main__':
     check_and_rename()
-
-# TODO
-# instead of copying have the file rename existing files
-
-#- browse the current directory (dir)
-#- check file extension
-#- if file extension type jpg or png
-#- operate on filenames and rename them.
