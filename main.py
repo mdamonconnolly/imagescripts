7548503398ea4e2e4dc12709178d4c47475543b7
c-lang18 <carolinalang96@gmail.com>
2019-09-30 22:49:46 +1000

    Refactoring this ugly code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, glob
 from PIL import Image
+import filetype
 
 def determine_ownership():
     dir = os.getcwd()
@@ -8,8 +9,24 @@ def determine_ownership():
         img = Image.open(filename)
         img.save('_MINE_' + str(filename))
 
+def renameFiles(checkFile.fType):
+    
+    for filename in os.listdir(os.getcwd()):
+        #os.rename(filename, '__MINE__' + filename)
+        print(filename)
+
+def checkFile():
+    fType = filetype.guess('bird.jpg')
+
+    if fType is None:
+        print('Error: File type is None')
+        return
+    print('File type: ' + fType.extension)
+
+
 if __name__=='__main__':
-    determine_ownership()
+    checkFile()
+
 ##working on 1 format
 # TODO
 # instead of copying have the file rename existing files
