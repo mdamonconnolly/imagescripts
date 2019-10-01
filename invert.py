de5e58bfba9d40e335e8dcd961cc4bf2749363e4
c-lang18 <carolinalang96@gmail.com>
2019-10-01 22:35:09 +1000

    Functioning code - still needs work
    - Code inverts and renames all cases correctly
    - Added test script to strip prefix (temporary - for quick testing)

--- invert.py
+++ invert.py
@@ -1,41 +1,55 @@
 import os
 import PIL
 from PIL import Image, ImageOps
+ 
+def invert():
+    imageExt = ".png", ".jpg"
+    overWriteQuery = input("Overwrite existing images? If 'N' selected, the inverted images will be saved as new files: \n Y | N\n")
+    currentDir = os.curdir
 
-def checkDir():
-    saveToDir = input("Save to new folder ('Y) or save to current directory ('N')?\n")
-    if saveToDir == 'Y' or saveToDir == 'y':
+    saveToDir = input("Save to new folder ('Y) or save to current directory (leave blank)?\n")
+    if saveToDir == 'y':
         if os.path.isdir('InvertedImages'):
             print('Folder already exists')
         else:
             newDir = os.mkdir('InvertedImages')
             print("New Folder Created\n")
 
-    elif saveToDir == 'N' or saveToDir == 'n':
-        pass
-        
-
-def invert():
-    imageExt = ".png", ".jpg"
-    promptInput = input("Overwrite existing images? If 'N' selected, the inverted images will be saved as new files: \n Y | N\n")
-
-    checkDir()
 
     for file in os.listdir("."):
         if file.endswith(imageExt):
             img = Image.open(file)
             invertColours = PIL.ImageOps.invert(img)
+            newfile = '_inverted_' + file
 
-            if promptInput == 'N' or promptInput == 'n':
-                invertColours.save('_Inverted_' + file)
-                
+            """
+            if '_inverted_' in file:
+                newfile = '_original_' + file
+                print(newfile)
+            if '_original_' in file:
+                newfile = '_inverted_' + file
+                print(newfile)
+            """
+            if overWriteQuery == 'n':
+                if saveToDir == 'y':
+                    invertColours.save(os.path.join(currentDir + '/InvertedImages', newfile))
+                else:
+                    invertColours.save(newfile)
                 print('Did not overwrite - saved copies')
 
-            elif promptInput == 'Y' or promptInput == 'y':
-                print(os.getcwd)
-                invertColours.save('_Inverted_' + file)
-                #os.remove(file)
-                print('Original files were overwritten with inverted version')
+            elif overWriteQuery == 'y':
+                if saveToDir == 'y':
+                    invertColours.save(os.path.join(currentDir + '/InvertedImages', newfile))
+                else:
+                    invertColours.save(newfile)
+                    os.remove(file)
+                    print('Original files were overwritten to this directory')
 
 if __name__=='__main__':
     invert()
+
+#TODO
+# check if inverted or original already prefixed to name - if so, swap them
+# refactor
+
+
