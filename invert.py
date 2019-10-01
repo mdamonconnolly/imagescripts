c65d80e964d4e4a97d25d3e88b91f9c7706e1f45
c-lang18 <carolinalang96@gmail.com>
2019-10-01 20:27:36 +1000

    Create initial inversion file

---
+++ invert.py
@@ -0,0 +1,17 @@
+import os
+from PIL import Image
+
+imageExt = ".png", ".jpg"
+
+def invert():
+    for file in os.listdir("."):
+        if file.endswith(imageExt):
+            img = Image.open(file)
+            invertColours = PIL.ImageOps.invert(img)
+            invertColours.save('_Inverted_' + file)
+            print('inverted')
+        else:
+            print('Error')
+
+if __name__=='__main__':
+    invert()
