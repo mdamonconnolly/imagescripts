a04986c804e6b4871046a287f96356deb8c1839e
c-lang18 <carolinalang96@gmail.com>
2019-10-01 21:28:36 +1000

    Update

--- invert.py
+++ invert.py
@@ -3,12 +3,16 @@ import PIL
 from PIL import Image, ImageOps
 
 def checkDir():
-    saveToDir = input("Save to new folder ('Y) or save to current directory ('N')?")
+    saveToDir = input("Save to new folder ('Y) or save to current directory ('N')?\n")
     if saveToDir == 'Y' or saveToDir == 'y':
-        if os.path.isdir('\InvertedImages'):
-            pass
+        if os.path.exists(str(os.getcwd) + '\InvertedImages'):
+            print('Folder already exists')
         else:
-            newDir = os.mkdir('InvertedImages')
+            newDir = os.mkdir('\InvertedImages')
+
+    elif saveToDir == 'N' or saveToDir == 'n':
+        pass
+        
 
 def invert():
     imageExt = ".png", ".jpg"
@@ -22,13 +26,13 @@ def invert():
             invertColours = PIL.ImageOps.invert(img)
 
             if promptInput == 'N' or promptInput == 'n':
-                invertColours.save(str(checkDir.newDir) + '\_Inverted_' + file)
+                invertColours.save('\InvertedImages\_Inverted_' + file)
                 
                 print('Did not overwrite - saved copies')
 
             elif promptInput == 'Y' or promptInput == 'y':
                 print(os.getcwd)
-                invertColours.save(str(checkDir.newDir) + '\_Inverted_' + file)
+                invertColours.save('\InvertedImages\_Inverted_' + file)
                 #os.remove(file)
                 print('Original files were overwritten with inverted version')
 
