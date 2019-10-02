7f7069438897117224b3284a865de9c5c19ccd75
c-lang18 <carolinalang96@gmail.com>
2019-10-02 23:59:31 +1000

    Skeleton code

--- translate.py
+++ translate.py
@@ -1,1 +1,23 @@
+import os
+from PIL import Image
 
+def initializeTranslate():
+    pass
+
+def scale():
+    pass
+
+def resize():
+    pass
+
+def flipHorizontal():
+    pass
+
+def flipVertical():
+    pass
+
+def rotate():
+    pass
+
+if __name__=='__main__':
+    initializeTranslate()
