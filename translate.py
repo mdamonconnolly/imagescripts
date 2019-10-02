4facf63cee4e00e017b670932dd2f6a888cc9bdb
c-lang18 <carolinalang96@gmail.com>
2019-10-03 00:26:17 +1000

    Beginning building-block structure

--- translate.py
+++ translate.py
@@ -1,13 +1,26 @@
 import os
 from PIL import Image
+import argparse
 
 def initializeTranslate():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--s', help='resize image to 0.25 original size', type=str)
+    parser.add_argument('--h', help='flip image horizontally', type=str)
+    parser.add_argument('--v', help='flip image vertically', type=str)
+    parser.add_argument('--r', help='rotate by {0}'.format(angle), type=str)
+
+    angle = 45
+    imageExt = ".png", ".jpg"
+
+
+
+    for file in os.listdir("."):
+        if file.endswith(imageExt):
+            img = Image.open(file)
+            #invertColours = PIL.ImageOps.invert(img)
+            #print('worked - opened {0}'.format(openFile))
 
 def scale():
-    pass
-
-def resize():
     pass
 
 def flipHorizontal():
