0537df2a427899474c0fbb8b2c4b0d08cb75e0f1
c-lang18 <carolinalang96@gmail.com>
2019-10-03 00:37:23 +1000

    Classed it up

--- translate.py
+++ translate.py
@@ -2,35 +2,49 @@ import os
 from PIL import Image
 import argparse
 
-def initializeTranslate():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--s', help='resize image to 0.25 original size', type=str)
-    parser.add_argument('--h', help='flip image horizontally', type=str)
-    parser.add_argument('--v', help='flip image vertically', type=str)
-    parser.add_argument('--r', help='rotate by {0}'.format(angle), type=str)
 
-    angle = 45
-    imageExt = ".png", ".jpg"
+class Translate():
+
+    def __init__(self):
+        
+        self.angle = 45
+        self.imageExt = ".png", ".jpg"
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--s', help='resize image to 0.25 original size', type=str)
+        parser.add_argument('--h', help='flip image horizontally', type=str)
+        parser.add_argument('--v', help='flip image vertically', type=str)
+        parser.add_argument('--r', help='rotate by {0}'.format(self.angle), type=str)
+
+        for self.file in os.listdir("."):
+            if self.file.endswith(self.imageExt):
+                self.img = Image.open(self.file)
+                #invertColours = PIL.ImageOps.invert(img)
+
+        self.u = input('Test input: ')
+        self.openFile()
+        
 
 
 
-    for file in os.listdir("."):
-        if file.endswith(imageExt):
-            img = Image.open(file)
-            #invertColours = PIL.ImageOps.invert(img)
-            #print('worked - opened {0}'.format(openFile))
+    def openFile(self):
+        print(self.u, self.file)
 
-def scale():
-    pass
+    def scale():
+        pass
 
-def flipHorizontal():
-    pass
+    def flipHorizontal():
+        pass
 
-def flipVertical():
-    pass
+    def flipVertical():
+        pass
 
-def rotate():
-    pass
+    def rotate():
+        pass
 
 if __name__=='__main__':
-    initializeTranslate()
+    init = Translate()
+
+# TODO
+# angle for rotation to be user-defined
+# scale to be user-defined - either by height/width or percentage
