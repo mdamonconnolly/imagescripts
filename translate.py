8298e66c05134c55fe400baa0b3a95038ed2136c
c-lang18 <carolinalang96@gmail.com>
2019-10-03 14:37:00 +1000

    update

--- translate.py
+++ translate.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--scale', help='resize image to 0.25 original size', action='store_true')
 parser.add_argument('-r', '--rotate', help='rotate image by 45 degrees', action='store_true')
-parser.add_argument('-h', '--horizontal', help='flip image horizontally', action='store_true')
+parser.add_argument('-hz', '--horizontal', help='flip image horizontally', action='store_true')
 parser.add_argument('-v', '--vertical', help='flip image vertically', action='store_true')
 args = parser.parse_args()
 
@@ -16,19 +16,20 @@ class Translate():
         self.angle = 45
         self.imageExt = ".png", ".jpg"
         self.openFile()
+        self.args = args
     
     def openFile(self):
         for self.file in os.listdir("."):
             if self.file.endswith(self.imageExt):
                 self.img = Image.open(self.file)
 
-        if self.args.s:
+        if self.args.scale:
             self.scale()
-        elif self.args.r:
+        elif self.args.rotate:
             self.rotate()
-        elif self.args.h:
+        elif self.args.horizontal:
             self.flipHorizontal()
-        elif self.args.v:
+        elif self.args.vertical:
             self.flipVertical()
         else:
             print('Error: invalid argument selected')
@@ -42,20 +43,18 @@ class Translate():
         print('Successfully resized {0}'.format(file) + ' to 25%. ')
 
     
-    def rotate():
+    def rotate(self):
         print('Rotate (empty)')
 
-    def flipHorizontal():
+    def flipHorizontal(self):
         print('Flip horizontal (empty)')
 
-    def flipVertical():
+    def flipVertical(self):
         print('Flip vertical (empty)')
 
 
-
-
-if __name__=='__main__':
-    init = Translate(args)
+#if __name__=='__main__':
+init = Translate(args)
 
 # TODO
 # angle for rotation to be user-defined
