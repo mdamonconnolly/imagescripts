04ecd84f2a6586fc95056011fe8c0421547a59b9
c-lang18 <carolinalang96@gmail.com>
2019-10-09 22:47:36 +1000

    Update resizer.py

--- resizer.py
+++ resizer.py
@@ -1,24 +1,19 @@
 import os
 from PIL import Image
 
-def resize():
+rawPercent = int(input("Please enter desired percentage to scale to\n"))
+path = os.getcwd()
+percent = rawPercent/100
 
-    rawPercent = int(input("Please enter desired percentage to scale to\n"))
-    path = os.getcwd()
-    percent = rawPercent/100
+for file in os.listdir("."):
+    if file.endswith(".png") or file.endswith(".jpg"):
+        newName = file.split('.')[0] + '_resized_{0}%.'.format(rawPercent) + file.split('.')[1]
 
-    for file in os.listdir("."):
-        if file.endswith(".png") or file.endswith(".jpg"):
-            newName = file.split('.')[0] + '_resized_{0}%.'.format(rawPercent) + file.split('.')[1]
+        img = Image.open(path + '\\' + file)
+        width = int(img.size[0] * percent)
+        height = int(img.size[1] * percent)
 
-            img = Image.open(path + '\\' + file)
-            width = int(img.size[0] * percent)
-            height = int(img.size[1] * percent)
+        img.thumbnail((width, height), Image.ANTIALIAS)
+        img.save(str(newName))
+        print('Successfully resized {0}'.format(file))
 
-            img.thumbnail((width, height), Image.ANTIALIAS)
-            print(img.size)
-            img.save(str(newName))
-            print('Successfully resized {0}'.format(file))
-
-if __name__=='__main__':
-    resize()
